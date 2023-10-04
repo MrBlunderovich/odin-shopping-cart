189e4402b8a899ae13ab065ed56868b67ed66a68
Maxim <334890@gmail.com>
2023-10-04 17:40:04 +0600

    edit todo list

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,8 +6,9 @@ export default function Home() {
       <pre>
         - make user sessions persistent
         <br />
-        - make cart refetches conditional, check staleness?
-        <br />- make successful login redirect user to previous page
+        - update cart item stock status somehow
+        <br />- let successful login redirect user to previous page
+        <br />- process failure to log in
       </pre>
     </div>
   );
